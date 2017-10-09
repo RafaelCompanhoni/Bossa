5c449b48d51137c296276b4202ed38a6b2283ede
Rafael Companhoni <companhoni@gmail.com>
2017-10-09 19:51:44 -0300

    Composing configuration: moved module.rules to webpack.parts and imported in the common config

--- config/webpack.config_OLD.js
+++ config/webpack.config_OLD.js
@@ -8,60 +8,31 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const VENDOR_LIBS = ['lodash'];
 
-const commonConfig = {
-  entry: {
-    bundle: './src/index.js',
-    vendor: VENDOR_LIBS,
-  },
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: '[name].[chunkhash].js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader',
-        }),
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: { limit: 40000 },
-          },
-          'image-webpack-loader',
-        ],
-      },
-      {
-        test: /\.js$/,
-        enforce: 'pre',
-        loader: 'eslint-loader',
-        exclude: /node_modules/,
-        options: {
-          emitWarning: true,
-        },
-      },
+const commonConfig = merge([
+  {
+    entry: {
+      bundle: './src/index.js',
+      vendor: VENDOR_LIBS,
+    },
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: '[name].[chunkhash].js',
+    },
+    plugins: [
+      new webpack.optimize.CommonsChunkPlugin({
+        name: ['vendor', 'manifest'],
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+      new ExtractTextPlugin('style.css'),
     ],
   },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: ['vendor', 'manifest'],
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-    new ExtractTextPlugin('style.css'),
-  ],
-};
+  parts.babel(),
+  parts.stylesheets(),
+  parts.images(),
+  parts.lintJavaScript(),
+]);
 
 const productionConfig = () => commonConfig;
 
--- config/webpack.parts.js
+++ config/webpack.parts.js
@@ -1,10 +1,61 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+exports.babel = () => ({
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+});
+
+exports.stylesheets = () => ({
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader',
+        }),
+      },
+    ],
+  },
+});
+
+exports.images = () => ({
+  module: {
+    rules: [
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 40000 },
+          },
+          'image-webpack-loader',
+        ],
+      },
+    ],
+  },
+});
+
 exports.lintJavaScript = () => ({
-  test: /\.js$/,
-  enforce: 'pre',
-  loader: 'eslint-loader',
-  exclude: /node_modules/,
-  options: {
-    emitWarning: true,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        loader: 'eslint-loader',
+        exclude: /node_modules/,
+        options: {
+          emitWarning: true,
+        },
+      },
+    ],
   },
 });
 
