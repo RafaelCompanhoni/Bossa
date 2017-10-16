a094c5e43fd435ab95395b862abe4b337feb848f
Rafael Companhoni <companhoni@gmail.com>
2017-10-15 23:12:51 -0200

    Example of a component requesting data via Ajax ('peopleList')

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 
 import Clock from './clock';
 import ColorPicker from './colorPicker';
+import PeopleList from './peopleList';
 
 const App = () => (
   <div>
     <Clock />
     <ColorPicker />
+    <PeopleList count={10} />
   </div>
 );
 
