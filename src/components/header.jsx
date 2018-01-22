fc8d88767a72b3e09447f5b4f20ea520392d93d2
Rafael Companhoni <companhoni@gmail.com>
2018-01-22 05:34:47 -0200

    Refactor to use BEM style

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,19 +4,19 @@ import styles from '../../styles/header.scss';
 
 const Header = () => (
   <div styleName="header">
-    <div styleName="logo-box">
-      <img styleName="logo" src="assets/img/logo-white.png" alt="Logo" />
+    <div styleName="header__logo-box">
+      <img styleName="header__logo" src="assets/img/logo-white.png" alt="Logo" />
     </div>
 
-    <div styleName="text-box">
+    <div styleName="header__text-box">
       <h1 styleName="heading-primary">
-        <span styleName="heading-primary-main">Outdoors</span>
-        <span styleName="heading-primary-sub">is where life happens</span>
+        <span styleName="heading-primary--main">Outdoors</span>
+        <span styleName="heading-primary--sub">is where life happens</span>
       </h1>
 
       {
         // eslint-disable-next-line
-      }<a href="#" styleName="btn-white">Discover our tours</a>
+      }<a href="#" styleName="btn--white">Discover our tours</a>
     </div>
   </div>
 );
