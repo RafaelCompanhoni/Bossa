c6a450afe42cc05cedf850482c25531a84747e66
Rafael Companhoni <companhoni@gmail.com>
2018-01-31 01:37:47 -0200

    About section initial (scss utilities and typography)!

--- src/pages/home/AboutSection.jsx
+++ src/pages/home/AboutSection.jsx
@@ -4,7 +4,7 @@ import styles from '../../../styles/pages/home/about-section.scss';
 
 const AboutSection = () => (
   <section styleName="section-about">
-    <div className="u-center-text u-margin-bottom-8">
+    <div className="u-center-text u-margin-bottom-big">
       <h2 styleName="heading-secondary">
         Exciting tours for adventurous people
       </h2>
@@ -12,8 +12,29 @@ const AboutSection = () => (
 
     <div className="row">
       <div className="col-1-of-2">
-        Text content
+        <h3 className="heading-tertiary u-margin-bottom-small">
+          You are going to fall in love with nature
+        </h3>
+
+        <p styleName="paragraph">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Excepturi nihil ea voluptates quas totam atque. Perferendis
+          perspiciatis porro officia sed facere ratione vero veritatis
+          cumque, quae, necessitatibus, omnis dolorum dignissimos!
+        </p>
+
+        <h3 className="heading-tertiary u-margin-bottom-small">
+          Live adventures like you never have before
+        </h3>
+
+        <p styleName="paragraph">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Excepturi nihil ea voluptates quas totam atque.
+        </p>
+
+        <a href="www.google.com" className="btn-text">Learn more &raar</a>
       </div>
+
       <div className="col-1-of-2">
         Image composition
       </div>
