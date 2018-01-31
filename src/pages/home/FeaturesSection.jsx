3797afde79c2781b78a3679c2bbcca3c8ffa8301
Rafael Companhoni <companhoni@gmail.com>
2018-01-31 19:42:56 -0200

    Features section styles

--- src/pages/home/FeaturesSection.jsx
+++ src/pages/home/FeaturesSection.jsx
@@ -4,7 +4,51 @@ import styles from '../../../styles/pages/home/features-section.scss';
 
 const FeaturesSection = () => (
   <section styleName="section-features">
-    <i className="icon-basic-world" />
+    <div className="row">
+      <div className="col-1-of-4" styleName="feature-box">
+        <i styleName="feature-box__icon" className="icon-basic-world" />
+
+        <h3 className="heading-tertiary u-margin-bottom-small">Explore the world</h3>
+
+        <p className="feature-box__text">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Excepturi nihil ea voluptates quas totam atque.
+        </p>
+      </div>
+
+      <div className="col-1-of-4" styleName="feature-box">
+        <i styleName="feature-box__icon" className="icon-basic-compass" />
+
+        <h3 className="heading-tertiary u-margin-bottom-small">Meet nature</h3>
+
+        <p className="feature-box__text">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Excepturi nihil ea voluptates quas totam atque.
+        </p>
+      </div>
+
+      <div className="col-1-of-4" styleName="feature-box">
+        <i styleName="feature-box__icon" className="icon-basic-map" />
+
+        <h3 className="heading-tertiary u-margin-bottom-small">Find your way</h3>
+
+        <p className="feature-box__text">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Excepturi nihil ea voluptates quas totam atque.
+        </p>
+      </div>
+
+      <div className="col-1-of-4" styleName="feature-box">
+        <i styleName="feature-box__icon" className="icon-basic-heart" />
+
+        <h3 className="heading-tertiary u-margin-bottom-small">Live a healthier life</h3>
+
+        <p className="feature-box__text">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Excepturi nihil ea voluptates quas totam atque.
+        </p>
+      </div>
+    </div>
   </section>
 );
 
